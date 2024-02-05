9d8860bc45b6a1a0dc6e0a305e61537ac0f91a4a
JORGE PEDRERO <jorgepedreror1@hotmail.com>
2024-02-05 14:58:46 +0900

    dev-wip passTrough Effect

---
+++ Assets/99.Testing/Jorge/PassTroughController.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static UnityEngine.InputSystem.InputAction;
+using UnityEngine.UI;
+public class PassTroughController : MonoBehaviour
+{
+    public OVRPassthroughLayer passtrough;
+    float passTAmount;
+    public float passSpeed = 0.1f;
+    public Vector3 color;
+    public bool edgeOn;
+    int currentColorType = 0;
+    public GameObject[] controllers;
+
+    public Slider contrast, brightness, saturation;
+    public Slider GrayScale_Contrast, Grayscale_Brightness, Grayscale_Posterize;
+    public Slider GrayScale2Color_Contrast, Grayscale2Color_Brightness, Grayscale2Color_Posterize;
+    public Slider ColorLut_Blend;
+    public Slider BlendedLut_Blend;
+
+    public void updateGraysCaleController()
+    {
+
+    }
+    // Start is called before the first frame update
+    void Start() 
+    {
+        passtrough = GetComponent<OVRPassthroughLayer>();
+        if (!passtrough)
+            Destroy(this);
+    }
+    public GameObject test;
+    public void updateColorСontrol()
+    {
+        currentColorType++;
+        if (currentColorType > 6) currentColorType = 0;
+        passtrough.colorMapEditorType = (OVRPassthroughLayer.ColorMapEditorType)currentColorType;
+        if (currentColorType<controllers.Length)
+        controllers[currentColorType].SetActive(true);
+    }
+    
+    public void RightStick(CallbackContext context)
+    {
+        var c = context.ReadValue<Vector2>();
+        updatePasstrough(c.y * passSpeed);
+        if(edgeOn)
+        {
+            color.x += c.x * passSpeed;
+            changeColor();
+
+        }
+    }
+    public void LeftStick(CallbackContext context)
+    {
+        var c = context.ReadValue<Vector2>();
+         
+        if (edgeOn)
+        {
+            color.y += c.x*passSpeed;
+            color.z += c.y*passSpeed;
+            changeColor();
+        }
+    }
+    public void OnAButton(CallbackContext context)
+    {
+         
+        if (test) test.SetActive(!test.activeInHierarchy);
+        toggleEdgeRendering(!edgeOn);
+    }
+    public void updatePasstrough(float passtroughAmount)
+    {
+        passTAmount += passtroughAmount;
+        passtrough.textureOpacity = passTAmount;
+    }
+    public void toggleEdgeRendering(bool value)
+    {
+        edgeOn = value;
+        passtrough.edgeRenderingEnabled = edgeOn;
+    }
+    public void changeColor()
+    {
+        passtrough.edgeColor = new Color(color.x,color.y,color.z);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/99.Testing/mapscameraController.cs
+++ Assets/99.Testing/mapscameraController.cs
@@ -1,20 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class mapscameraController : MonoBehaviour
 {
     public Vector3 speed;
+    public float timeToLoadScene=60f;
+    public int sceneToLoad = 1;
     // Start is called before the first frame update
     void Start()
     {
-        
+        OVRManager.suggestedGpuPerfLevel = OVRManager.ProcessorPerformanceLevel.Boost;
+        OVRManager.suggestedCpuPerfLevel = OVRManager.ProcessorPerformanceLevel.Boost;
+        StartCoroutine(loadScene());
+    }
+
+    IEnumerator loadScene()
+    {
+        yield return new WaitForSeconds(timeToLoadScene);
+        SceneManager.LoadScene(sceneToLoad);
     }
 
     // Update is called once per frame
     void Update()
-    {
-        transform.position += speed;
+    { 
         
     }
 }
